00dd840ea014b98ac26bb4587b584feeb38f4a4a
Leandro Stanzione <stanzione.leandro@gmail.com>
2016-02-22 20:59:19 -0300

    Finished Sign Up and Sign In elements, flow and animations

--- app/src/main/java/com/stanzione/loginprototype/LoginActivity.java
+++ app/src/main/java/com/stanzione/loginprototype/LoginActivity.java
@@ -13,28 +13,45 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private CoordinatorLayout coordinatorLayout;
 
+    private Button loginSignInButton;
     private Button loginSignUpButton;
     private TextView titleTextView;
 
+    //Sign In elements
+    private RelativeLayout signInLayout;
+    private EditText signInUsernameEditText;
+    private EditText signInPasswordEditText;
+    private Button signInButton;
+
+    //Sign Up elements
     private CardView signUpCardView;
     private EditText signUpUsernameEditText;
     private EditText signUpEmailEditText;
     private EditText signUpPasswordEditText;
     private Button signUpButton;
 
+    private LoginState currentState = LoginState.STATE_LOGO;
+
+    private SignInRequest signInRequest;
     private SignUpRequest signUpRequest;
 
+    private boolean isSignInRunning = false;
     private boolean isSignUpRunning = false;
 
     Animation animFadeIn;
     Animation animFadeOut;
     Animation animExplode;
+    Animation animEnterFromLeft;
+    Animation animEnterFromRight;
+    Animation animExitToLeft;
+    Animation animExitToRight;
 
     private enum LoginState {
         STATE_LOGO,
@@ -53,11 +70,21 @@ public class LoginActivity extends AppCompatActivity {
         animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
         animFadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
         animExplode = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.explode);
+        animEnterFromLeft = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.enter_from_left);
+        animEnterFromRight = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.enter_from_right);
+        animExitToLeft = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.exit_to_left);
+        animExitToRight = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.exit_to_right);
 
         coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
 
+        loginSignInButton = (Button) findViewById(R.id.loginButton);
         loginSignUpButton = (Button) findViewById(R.id.loginSignUpButton);
         titleTextView = (TextView) findViewById(R.id.spreadTitle);
+
+        signInLayout = (RelativeLayout) findViewById(R.id.signInLayout);
+        signInUsernameEditText = (EditText) findViewById(R.id.signInUsernameEditText);
+        signInPasswordEditText = (EditText) findViewById(R.id.signInPasswordEditText);
+        signInButton = (Button) findViewById(R.id.signInButton);
 
         signUpCardView = (CardView) findViewById(R.id.signUpCardView);
         signUpUsernameEditText = (EditText) findViewById(R.id.signUpUsernameEditText);
@@ -65,10 +92,24 @@ public class LoginActivity extends AppCompatActivity {
         signUpPasswordEditText = (EditText) findViewById(R.id.signUpPasswordEditText);
         signUpButton = (Button) findViewById(R.id.signUpButton);
 
+        loginSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignIn();
+            }
+        });
+
         loginSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showSignUp();
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doSignIn();
             }
         });
 
@@ -81,30 +122,134 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void showSignUp(){
+    private void showSignIn(){
 
         if(signUpRequest != null) {
             signUpRequest.cancel(true);
         }
 
-        titleTextView.startAnimation(animExplode);
+        if(currentState.equals(LoginState.STATE_LOGO)) {
 
-        titleTextView.getAnimation().setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
+            currentState = LoginState.STATE_SIGNIN;
 
-            }
+            titleTextView.startAnimation(animExplode);
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                signUpCardView.startAnimation(animFadeIn);
-            }
+            titleTextView.getAnimation().setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
 
-            @Override
-            public void onAnimationRepeat(Animation animation) {
+                }
 
-            }
-        });
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    signInLayout.startAnimation(animFadeIn);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+        }
+        else if(currentState.equals(LoginState.STATE_SIGNUP)){
+
+            currentState = LoginState.STATE_SIGNIN;
+
+            signInLayout.startAnimation(animEnterFromLeft);
+            signUpCardView.startAnimation(animExitToRight);
+
+            signUpCardView.getAnimation().setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    signUpCardView.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+        }
+
+    }
+
+    private void showSignUp(){
+
+        if(signInRequest != null) {
+            signInRequest.cancel(true);
+        }
+
+        if(currentState.equals(LoginState.STATE_LOGO)) {
+
+            currentState = LoginState.STATE_SIGNUP;
+
+            titleTextView.startAnimation(animExplode);
+
+            titleTextView.getAnimation().setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    signUpCardView.startAnimation(animFadeIn);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+        }
+        else if(currentState.equals(LoginState.STATE_SIGNIN)){
+
+            currentState = LoginState.STATE_SIGNUP;
+
+            signInLayout.startAnimation(animExitToLeft);
+            signUpCardView.startAnimation(animEnterFromRight);
+
+            signInLayout.getAnimation().setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    signInLayout.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+        }
+
+    }
+
+    private void doSignIn(){
+
+        if(!isSignInUsernameValid()){
+            Snackbar.make(coordinatorLayout, "Username must have at least 5 characters", Snackbar.LENGTH_SHORT).show();
+        }
+        else if(!isSignInPasswordValid()){
+            Snackbar.make(coordinatorLayout, "Enter a password", Snackbar.LENGTH_SHORT).show();
+        }
+        else{
+            signInRequest = new SignInRequest();
+            signInRequest.execute(new String[]{getSignInUsername(), getSignInPassword()});
+        }
 
     }
 
@@ -148,6 +293,70 @@ public class LoginActivity extends AppCompatActivity {
 
     private String getPassword(){
         return signUpPasswordEditText.getText().toString().trim();
+    }
+
+    private boolean isSignInUsernameValid(){
+        return getSignInUsername().length() >= 5;
+    }
+
+    private boolean isSignInPasswordValid(){
+        return !getSignInPassword().isEmpty();
+    }
+
+    private String getSignInUsername(){
+        return signInUsernameEditText.getText().toString().trim();
+    }
+
+    private String getSignInPassword(){
+        return signInPasswordEditText.getText().toString().trim();
+    }
+
+    private class SignInRequest extends AsyncTask<String, Void, Boolean>{
+
+        @Override
+        protected Boolean doInBackground(String... values) {
+
+            String username = values[0];
+            String password = values[1];
+
+            //acting as a 10 second server communication
+
+            try{
+                for(int i=0; i<10; i++){
+                    Log.d("TAG", "i = " + i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //acting as a successfully login attempt
+            if(username.equalsIgnoreCase("lstanzione")){
+                return true;
+            }
+
+            return false;
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean signInResult) {
+
+            if(signInResult){
+                Snackbar.make(coordinatorLayout, "Logged successfully!", Snackbar.LENGTH_SHORT).show();
+            }
+            else{
+                Snackbar.make(coordinatorLayout, "Wrong username or password!", Snackbar.LENGTH_LONG).show();
+            }
+
+        }
+
+        @Override
+        protected void onCancelled() {
+
+            Snackbar.make(coordinatorLayout, "Sign In cancelled!", Snackbar.LENGTH_SHORT).show();
+
+        }
     }
 
     private class SignUpRequest extends AsyncTask<String, Void, Boolean>{
